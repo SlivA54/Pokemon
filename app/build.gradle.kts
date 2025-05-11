88134a279869e314fc4d77ff1c31c141dd6356f9
SlivA54 <gavrisevdanil765@gmail.com>
2025-05-12 00:05:30 +0700

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.pokemon"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.pokemon"
-        minSdk = 34
-        targetSdk = 34
+        minSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
